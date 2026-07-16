606213b3840c8f63625cd8bb959f8815313cf527
Jerry Hor <jhor@u82z03s04.42kl.edu.my>
2026-07-16 11:02:35 +0800

    correct test case in main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -9,14 +9,14 @@ int main(void)
 	ScavTrap bob2(bob);
 	std::cout << "bob2's hitpoints: " << bob.getstats("_Hitpoints") << std::endl;
 	std::cout << "bob2's energy_point: " << bob.getstats("_Energy_points") << std::endl;
-	std::cout << "bob2's attack_damage: " << bob.getstats("_attack_damage") << std::endl;
+	std::cout << "bob2's attack_damage: " << bob.getstats("_Attack_damage") << std::endl;
 	std::cout << "bob2's type: " << bob.getNameType("_Type") << std::endl;
 
 	ScavTrap bob3("Bob3");
 	bob3 = bob2;
 	std::cout << "bob3's hitpoints: " << bob.getstats("_Hitpoints") << std::endl;
 	std::cout << "bob3's energy_point: " << bob.getstats("_Energy_points") << std::endl;
-	std::cout << "bob3's attack_damage: " << bob.getstats("_attack_damage") << std::endl;
+	std::cout << "bob3's attack_damage: " << bob.getstats("_Attack_damage") << std::endl;
 	std::cout << "bob3's type: " << bob.getNameType("_Type") << std::endl;
 
 	std::cout << "\n--- test attack ---" << std::endl;
@@ -38,15 +38,15 @@ int main(void)
 	std::cout << "\n--- stats ---" << std::endl;
 	std::cout << "bob's hitpoints: " << bob.getstats("_Hitpoints") << std::endl;
 	std::cout << "bob's energy_point: " << bob.getstats("_Energy_points") << std::endl;
-	std::cout << "bob's attack_damage: " << bob.getstats("_attack_damage") << std::endl;
+	std::cout << "bob's attack_damage: " << bob.getstats("_Attack_damage") << std::endl;
 	std::cout << "bob's type: " << bob.getNameType("_Type") << std::endl << std::endl;
 	std::cout << "bob2's hitpoints: " << bob2.getstats("_Hitpoints") << std::endl;
 	std::cout << "bob2's energy_point: " << bob2.getstats("_Energy_points") << std::endl;
-	std::cout << "bob2's attack_damage: " << bob2.getstats("_attack_damage") << std::endl;
+	std::cout << "bob2's attack_damage: " << bob2.getstats("_Attack_damage") << std::endl;
 	std::cout << "bob2's type: " << bob2.getNameType("_Type") << std::endl << std::endl;
 	std::cout << "Timmy's hitpoints: " << Timmy.getstats("_Hitpoints") << std::endl;
 	std::cout << "Timmy's energy_point: " << Timmy.getstats("_Energy_points") << std::endl;
-	std::cout << "Timmy's attack_damage: " << Timmy.getstats("_attack_damage") << std::endl;
+	std::cout << "Timmy's attack_damage: " << Timmy.getstats("_Attack_damage") << std::endl;
 	std::cout << "Timmy's type: " << Timmy.getNameType("_Type") << std::endl;
 
 	std::cout << "\n--- test gate keeper mode ---" << std::endl;
